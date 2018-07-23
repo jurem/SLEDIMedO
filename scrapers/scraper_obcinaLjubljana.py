cb9565e3bf20502b88be07bc090b5e3141669f63
markzakelj <markzakelj@gmail.com>
2018-07-23 18:05:31 +0200

    update

--- scrapers/scraper_obcinaLjubljana.py
+++ scrapers/scraper_obcinaLjubljana.py
@@ -12,8 +12,10 @@ base_url = 'https://www.ljubljana.si'
 full_url = 'https://www.ljubljana.si/sl/aktualno/?start=' #kasneje dodas se stevilo zacetka (10, 20, ...)
 headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
 
+
 def makeHash(title, date):
     return hashlib.sha1((title+date).encode('utf-8')).hexdigest()
+
 
 def isArticleNew(hash):
     is_new = False
@@ -28,6 +30,7 @@ def isArticleNew(hash):
         print('new article found')
     f.close()
     return is_new
+
 
 def getLink(soup):
     link = soup.select('div > h2 > a')
@@ -48,12 +51,14 @@ def getDate(soup):
     print('date not found, update select() method')
     return 'date not found'
 
+
 def getTitle(soup):
     title = soup.select('div > h2 > a')
     if title:
         return title[0].text
     print('title not found, update select() method')
     return 'title not found'
+
 
 def getContent(url, session):
     r = session.get(url, timeout=10)
@@ -66,9 +71,11 @@ def getContent(url, session):
     print('content not found, update select() method')
     return 'content not found'
 
+
 def makeNewFile(link, title, date, content, hash):
     with open(hash + '.txt', 'w+', encoding='utf-8') as info_file:
         info_file.write(link + '\n' + title + '\n' + date + '\n' + content)
+
 
 def getArticlesOn_n_pages(num_pages_to_check, session):
 
@@ -82,16 +89,13 @@ def getArticlesOn_n_pages(num_pages_to_check, session):
 
     return articles
 
+
 def main():
     num_pages_to_check = 1
     num_new_articles = 0
-    articles_checked = 0
 
     with requests.Session() as session:
         session.headers.update(headers)
-        r = session.get(full_url + str(0), timeout=10)
-        soup = BeautifulSoup(r.text, 'html.parser')
-
         articles = getArticlesOn_n_pages(num_pages_to_check, session)
         articles_checked = len(articles)
 
