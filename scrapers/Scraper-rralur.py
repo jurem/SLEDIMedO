61f3fc032d0d8b8a7d72a747d543ddd5d003bdcf
Timotej Petric <timopetric@hotmail.com>
2018-08-03 10:41:31 +0200

    complete update, now it works

--- scrapers/Scraper-rralur.py
+++ scrapers/Scraper-rralur.py
@@ -1,99 +1,123 @@
+# -*- coding: utf-8 -*-
+
 import bs4 as bs
 import requests
 import re
 import hashlib
 import os.path
+import sys # for arguments
+import datetime
+from database.dbExecutor import dbExecutor
 
-DEBUG = True
+"""
+    no need to check multiple pages, every article link is on the same page.
+"""
+
+SOURCE_ID = "RRALUR"
+MAX_HTTP_RETRIES = 10   # set max number of http request retries if a page load fails
 BASE_URL = "http://www.rralur.si"
-	
+DEBUG = True
+    
 def makeHash(articleTitle, dateStr):
-	hash_object = hashlib.sha1(str(articleTitle)+str(dateStr))
-	return hash_object.hexdigest()
+    hash_object = hashlib.sha1((articleTitle+dateStr).encode('utf-8'))
+    return hash_object.hexdigest()
 
 def parseDate(toParse):
-	regexStr = "\\s+?(\\d{2}\\.\\s\\d{2}\\.\\s\\d{4}).*?"
-	result = re.search(regexStr, toParse, re.M|re.U|re.I)
-	if result is None:
-		if DEBUG: print (("Date not specified/page is different"))
-		return None
-	return result.group(1)
+    regexStr = "\\s+?(\\d{2}\\.\\s\\d{2}\\.\\s\\d{4}).*?"
+    result = re.search(regexStr, toParse, re.M|re.U|re.I)
+    if result is None:
+        if DEBUG: print (("Date not specified/page is different"))
+        return None
+    return result.group(1)
 
 def parseLink(toParse):
-	regexStr = "=\\s'(.*?)'"
-	result = re.search(regexStr, toParse, re.M|re.U|re.I)
-	if result is None:
-		if DEBUG: print (("Page is different"))
-		return None
-	return BASE_URL+result.group(1)
+    regexStr = "=\\s'(.*?)'"
+    result = re.search(regexStr, toParse, re.M|re.U|re.I)
+    if result is None:
+        if DEBUG: print (("Page is different"))
+        return None
+    return BASE_URL+result.group(1)
 
 def parseTitle(toParse):
-	regexStr = "^\\s+(.*?)\\s+$"
-	result = re.search(regexStr, toParse, re.M|re.U|re.I)
-	if result is None:
-		if DEBUG: print ("Page is different")
-		return None
-	return result.group(1)
+    regexStr = "^\\s+(.*?)\\s+$"
+    result = re.search(regexStr, toParse, re.M|re.U|re.I)
+    if result is None:
+        if DEBUG: print ("Page is different")
+        return None
+    return result.group(1)
 
 # navigates to the given link and extracts the article description
 def getArticleDescr(session, link):
-	resp = session.get(link)
-	soup = bs.BeautifulSoup(resp.text, "html.parser")
-	return soup.find("div", class_="field-item even").text
+    resp = session.get(link)
+    soup = bs.BeautifulSoup(resp.text, "html.parser")
+    return soup.find("div", class_="field-item even").text
+
+# creates a uniform date string out of the input @dateStr and date format @inputDateFromat
+# input format defaulted to: "%d.%m.%Y"
+# output format: "%Y-%m-%d" - default database entry format
+def uniformDateStr(dateStr, inputDateFromat=""):
+    if inputDateFromat == "":
+        inputDateFromat = "%d.%m.%Y"
+    return datetime.datetime.strptime(dateStr, inputDateFromat).strftime("%Y-%m-%d")
 
 
 def main():
-	listOfPages = list()	# contains all info about articles [title, link, dateStr]
-	articlesChecked = 0
+    articlesChecked = 0     # number of checked articles
+    articlesDownloaded = 0  # number of downloaded articles
 
-	with requests.Session() as s:
-		pageStart = 0
-		resp = s.get(BASE_URL+"/sl/news")
-		soup = bs.BeautifulSoup(resp.text, "html.parser")
+    # optionally set headers for the http request
+    HEADERS = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+    }
 
-		# print (resp.text)
-		# find all ~15 articles on current page
-		articles = soup.find_all("div", class_="grid-item")
+    sqlBase = dbExecutor()  # creates a sql database handler class
+    todayDateStr = datetime.datetime.now().strftime("%Y-%m-%d") # today date in the uniform format
 
-		for article in articles:
-			articlesChecked += 1
+    with requests.Session() as s:
 
+        # set every http/https request to retry max MAX_HTTP_RETRIES retries before returning an error if there is any complications with loading the page
+        s.mount("http://", requests.adapters.HTTPAdapter(max_retries = MAX_HTTP_RETRIES))  # set max retries to: MAX_HTTP_RETRIES
+        s.mount("https://", requests.adapters.HTTPAdapter(max_retries = MAX_HTTP_RETRIES)) # set max retries to: MAX_HTTP_RETRIES
+        s.headers.update(HEADERS)   # set headers of the session
 
-			title = parseTitle(article.find("h3", class_="title").text)
-			print title
-			link = parseLink(article["onclick"])
-			print link
+        resp = s.get(BASE_URL+"/sl/news")
+        soup = bs.BeautifulSoup(resp.text, "html.parser")
 
-			dateStr = parseDate(article.find("div", class_="news-date").text)
-			print dateStr
-			hashStr = makeHash(title.encode("utf-8"), dateStr.encode("utf-8"))
+        articles = soup.find_all("div", class_="grid-item")
 
+        for article in articles:
+            articlesChecked += 1
+            try:
+                title = parseTitle(article.find("h3", class_="title").text)
+                link = parseLink(article["onclick"])
+                dateStr = parseDate(article.find("div", class_="news-date").text)
+                hashStr = makeHash(title, dateStr)   
 
-			# if file does not yet exitst or filesize is == 0 we create it
-			if not os.path.isfile(hashStr+".txt") or os.path.isfile(hashStr+".txt") and os.stat(hashStr+".txt").st_size == 0:
-				description = getArticleDescr(s, link)
-
-
-				# print (title)
-				# print (link)
-				# print (dateStr)
-				# print (hashStr )
-				listOfPages.append([title, description, link, dateStr, hashStr])
-
-				#print (title+"\n"+link+"\n"+description+"\n"+dateStr+"\n")
-				with open(hashStr+".txt", "w") as f:
-					f.write((link+"\n"+title+"\n"+description+"\n"+dateStr+"\n").encode("utf-8"))
-
-				# TODO: upload data to the database (sqlite)
-
-			if DEBUG and articlesChecked % 5 == 0:
-				print ("Checked:", articlesChecked, "articles.")
+                # print ("date created:", dateStr)
+                date_created = uniformDateStr(dateStr, "%d. %m. %Y") # date when the article was published on the page
+                date_downloaded = todayDateStr                       # date when the article was downloaded
 
 
+                # if article is not yet saved in the database we add it
+                if sqlBase.getByHash(hashStr) is None:
+                    # get article description/content
+                    description = getArticleDescr(s, link)
 
-	print ("NOVI CLANKI:")
-	for article in listOfPages:
-		print (article)
+                    # (date_created: string, caption: string, contents: string, date: string, hash: string, url: string, source: string)
+                    entry = (date_created, title, description, date_downloaded, hashStr, link, SOURCE_ID)
+                    sqlBase.insertOne(entry)   # insert the article in the database
+                    articlesDownloaded += 1
+
+                if DEBUG and articlesChecked % 5 == 0:
+                    print ("Checked:", articlesChecked, "articles.")
+            except Exception as e:
+                print (e)
+
+
+    print ("Downloaded:", articlesDownloaded, "new articles.")
+    # print (sqlBase.getById(2))
 
 if __name__ == '__main__':
-	main()
+    print ("No need for the -F argument, as there is always just one page to check all the articles.")
+
+    main()
