45c392fc02353b4e3bc6a1630fb3b2a408dcef83
Timotej Petric <timopetric@hotmail.com>
2018-08-04 21:25:21 +0200

    added speed improvements for the first time load

--- scrapers/Scraper-twitter.py
+++ scrapers/Scraper-twitter.py
@@ -11,12 +11,16 @@ from database.dbExecutor import dbExecutor
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome() 
-driver.set_window_position(-10000,0)    # hides the browser
+# driver.set_window_position(-10000,0)    # hides the browser
 driver.implicitly_wait(15)
 
-ACCOUNT_NAMES = ["LjTehnoloski", "ZrcSazu", "MO_Velenje", "urbaninstitut",
+ACCOUNT_NAMES = [
+                ["hashtag/GeoZS?src=hash", "hashtag/DriDanube?src=hash",
+                "hashtag/EcoKarst?src=hash", "hashtag/rralur?src=hash"],
+                ["LjTehnoloski", "ZrcSazu", "MO_Velenje", "urbaninstitut",
                 "statslovenija", "mzi_rs", "ulfggljubljana", "MO_Velenje",
                 "skupnost_obcin", "ZRSBistra", "SrceSlovenije", "poligonsi",
                 "CEDSlovenia", "SrceSlovenije", "RAZUMsi", "visitidrija",
@@ -27,10 +31,10 @@ ACCOUNT_NAMES = ["LjTehnoloski", "ZrcSazu", "MO_Velenje", "urbaninstitut",
                 "gzsnovice", "poklicno", "bsckranjsi", "mzi_rs", "univerzam",
                 "gzsnovice", "turizemslovenia", "zrcsazu", "zvkds",
                 "FundacijaPRIZMA", "univerzam", "obcinaruse", "bsckranjsi",
-                "PomurjeTP", "pomurjer", "mzi_rs", "mra_po"]
+                "PomurjeTP", "pomurjer", "mzi_rs", "mra_po"]]
 
 SOURCE_ID = "TWITTER"        # source identifier
-NUM_JUMPS_TO_END_TO_MAKE = 50 # how many pages will we check evey day for new articles
+NUM_JUMPS_TO_END_TO_MAKE = 400 # how many pages will we check evey day for new articles
 BASE_URL = "https://twitter.com"
 DEBUG = True
 
@@ -56,57 +60,95 @@ def main():
     sqlBase = dbExecutor()  # creates a sql database handler class
     todayDateStr = datetime.datetime.now().strftime("%Y-%m-%d") # today date in the uniform format
     yearInt = datetime.datetime.now().year
+    errorList = list()
 
     numJumpsToMake = NUM_JUMPS_TO_END_TO_MAKE
     if not firstRunBool:
         numJumpsToMake = 2
-    
-    for accountName in ACCOUNT_NAMES:
-        sourceId = SOURCE_ID+"-"+accountName.upper()    # field "SOURCE" in the database
 
-        print("Scraping for:", accountName)
-        link = BASE_URL+"/"+accountName
-        driver.get(link)
+    for pageId, pageType in enumerate(ACCOUNT_NAMES):
+        for accountName in pageType:
+            try:
+                sourceId = SOURCE_ID+"-"+accountName.upper()    # field "SOURCE" in the database
+                if "hashtag/" in accountName[:9]:
+                    sourceId = SOURCE_ID+"-#"+accountName[8:-9].upper()    # field "SOURCE" in the database
+                    print (sourceId)
 
-        # scroll to the end of page numJumpsToMake times
-        for jump in range(1, numJumpsToMake+1):
-            elem = driver.find_element_by_tag_name('a')
-            elem.send_keys(Keys.END)
-            print (str(jump)+"/"+str(numJumpsToMake), "jumps to the end of the page completed.")
-            time.sleep(2)
+                print("Scraping for:", accountName)
+                link = BASE_URL+"/"+accountName
+                driver.get(link)
 
-        # print (driver.page_source.encode("utf-8"))
+                # scroll to the end of page numJumpsToMake times
+                scrollHeightBefore = 0
+                scrollHeightNow = 0
+                numTimesHeightEqual = 0
+                for jump in range(1, numJumpsToMake+1):
+                    elem = driver.find_element_by_tag_name('a')
+                    elem.send_keys(Keys.END)
+                    print (str(jump)+"/"+str(numJumpsToMake), "jumps to the end of the page completed.")
 
-        soup = bs.BeautifulSoup(driver.find_elements_by_tag_name('ol')[1].get_attribute('innerHTML'),'html.parser')
+                    scrollHeightNow = int(driver.execute_script("return document.body.scrollHeight"))
+                    if (scrollHeightBefore == scrollHeightNow):
+                        numTimesHeightEqual += 1
+                        if numTimesHeightEqual >= 3:
+                            print ("All tweets loaded or so it seams.")
+                            break
+                    scrollHeightBefore = scrollHeightNow
+                    time.sleep(2)
+                    # print (driver.find_element(By.XPATH, '//button[text()="Back to top ↑"]').get_attribute('innerHTML').encode("utf-8"))
 
-        for tweet in extractTweetTags(soup):
-            title = tweet.find("span", class_="FullNameGroup").text
-            description = tweet.find("div", class_="js-tweet-text-container").text
-            link = BASE_URL+tweet.find("small", class_="time").find("a")["href"]
+                # print (driver.page_source.encode("utf-8"))
+                time.sleep(3)
 
-            timeStamp = tweet.find("small", class_="time").find("a").find("span")["data-time-ms"]
-            date_created = datetime.datetime.fromtimestamp(int(timeStamp)/1000).strftime('%Y-%m-%d')
-            hashStr = makeHash(title, date_created)
+                # pageId part is a coincidence
+                soup = bs.BeautifulSoup(driver.find_elements_by_tag_name('ol')[pageId].get_attribute('innerHTML'),'html.parser')
 
-            date_downloaded = todayDateStr  # date when the article was downloaded
-            
-            # print("CONTENT:", description)
-            # print ("TITLE:", title)
-            # print ("URL:", link)
-            # print ("DATE:", dateStr)
+                for tweet in extractTweetTags(soup):
+                    title = tweet.find("span", class_="FullNameGroup").text
+                    description = tweet.find("div", class_="js-tweet-text-container").text
+                    link = BASE_URL+tweet.find("small", class_="time").find("a")["href"]
 
-            # if article is not yet saved in the database we add it
-            if sqlBase.getByHash(hashStr) is None:
-                # (date_created: string, caption: string, contents: string, date: string, hash: string, url: string, source: string)
-                entry = (date_created, title, description, date_downloaded, hashStr, link, sourceId)
-                sqlBase.insertOne(entry)   # insert the article in the database
-                tweetsDownloaded += 1
+                    timeStamp = tweet.find("small", class_="time").find("a").find("span")["data-time-ms"]
+                    date_created = datetime.datetime.fromtimestamp(int(timeStamp)/1000).strftime('%Y-%m-%d')
+                    hashStr = makeHash(title, date_created)
 
-                if DEBUG and tweetsDownloaded % 5 == 0 and tweetsDownloaded != 0:
-                    print ("Downloaded:", tweetsDownloaded, "new tweets.")
+                    date_downloaded = todayDateStr  # date when the article was downloaded
+                    
+                    # print("CONTENT:", description)
+                    # print ("TITLE:", title)
+                    # print ("URL:", link)
+                    # print ("DATE:", dateStr)
+
+                    printBool = False
+                    # if article is not yet saved in the database we add it
+                    if sqlBase.getByHash(hashStr) is None:
+                        # (date_created: string, caption: string, contents: string, date: string, hash: string, url: string, source: string)
+                        entry = (date_created, title, description, date_downloaded, hashStr, link, sourceId)
+                        sqlBase.insertOne(entry)   # insert the article in the database
+                        tweetsDownloaded += 1
+                        printBool = True
+
+                    if printBool and DEBUG and tweetsDownloaded % 5 == 0 and tweetsDownloaded != 0:
+                        print ("Downloaded:", tweetsDownloaded, "new tweets.")
+            except Exception as e:
+                print (e)
+                errorList.append(e)
+                errorList.append("Downloaded:", tweetsDownloaded, "new tweets.")
+            # finally:
+            #     if driver:
+            #         driver.quit()
 
     print ("Downloaded:", tweetsDownloaded, "new tweets.")
     driver.quit()
 
 if __name__ == '__main__':
+    # checks if the second argument is provided and is equal to "-F" - means first run
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "-F":
+            firstRunBool = True
+        else:
+            firstRunBool = False
+
+    print ("Add -F as the command line argument to execute first run command - downloads the whole history of articles from the page.\nWARNING: -F option will take a lot of time")
+
     main()
