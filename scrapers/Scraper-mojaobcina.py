13d6ba6789409fef63c0b601f0510519b175b1c2
Timotej Petric <timopetric@hotmail.com>
2018-08-09 13:09:53 +0200

    new scrapers for mojaobcina and razvoj

---
+++ scrapers/Scraper-mojaobcina.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+
+import bs4 as bs
+import requests
+import re
+import hashlib
+import os.path
+import sys # for arguments
+import datetime
+from logLoader import loadLogger
+from database.dbExecutor import dbExecutor
+
+SOURCE_ID = "MOJAOBCINA" # source identifier
+NUM_PAGES_TO_CHECK = 3  # how many pages will we check evey day for new articles
+MAX_HTTP_RETRIES = 10   # set max number of http request retries if a page load fails
+
+BASE_URL = "https://www.mojaobcina.si"
+POSTS_URL = BASE_URL+"/litija/novice/iskalnik/?order=date&not-use=ASC&page="
+
+firstRunBool = False    # import all the articles that exist if true; overrides NUM_PAGES_TO_CHECK
+
+logger = loadLogger(SOURCE_ID)
+
+# makes a sha1 hash string from atricle title and date string
+# returns string hash
+def makeHash(articleTitle, dateStr):
+    hash_object = hashlib.sha1((articleTitle+dateStr).encode("utf-8"))
+    return hash_object.hexdigest()
+
+# parse date from html
+def parseDate(toParseStr):
+    dateRegex = ".*?(\\d{2}\\.\\d{2}\\.\\d{4}).*?"
+    dateResult = re.search(dateRegex, toParseStr, re.M|re.U|re.I)
+    if dateResult is None:
+        # raise Exception("Date not specified/page is different")
+        logger.error("Date not specified/page is different")
+        return None
+    return dateResult.group(1)
+
+# navigates to the given link and extracts the article description
+# returns article description string
+def getArticleDescr(session, link):
+    resp = session.get(link)
+    soup = bs.BeautifulSoup(resp.text, "html.parser")
+    articleDate = parseDate(soup.find("div", class_="single-art-date fl").text)
+    subArt = soup.find("div", class_="single-art-sub-title")
+    artText = soup.find("div", class_="single-art-text")
+    [x.extract() for x in subArt.find_all("div", class_="articleVote")]
+    [x.extract() for x in artText.find_all("div", class_="articleVote")]
+    return (subArt.text+"\n"+artText.text, articleDate)
+
+# creates a uniform date string out of the input @dateStr and date format @inputDateFromat
+# input format defaulted to: "%d.%m.%Y"
+# output format: "%Y-%m-%d" - default database entry format
+def uniformDateStr(dateStr, inputDateFromat=""):
+    if inputDateFromat == "":
+        inputDateFromat = "%d.%m.%Y"
+    return datetime.datetime.strptime(dateStr, inputDateFromat).strftime("%Y-%m-%d")
+
+
+# main function
+def main():
+    pagesChecked = 0       # number of checked pages
+    articlesChecked = 0    # number of checked articles
+    articlesDownloaded = 0 # number of downloaded articles
+
+    # optionally set headers for the http request
+    HEADERS = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+    }
+
+    sqlBase = dbExecutor()  # creates a sql database handler class
+    todayDateStr = datetime.datetime.now().strftime("%Y-%m-%d") # today date in the uniform format
+
+    # creates a session
+    with requests.Session() as s:
+        pageStart = 1    # set at which page (article) to start
+
+        # set every http/https request to retry max MAX_HTTP_RETRIES retries before returning an error if there is any complications with loading the page
+        s.mount("http://", requests.adapters.HTTPAdapter(max_retries = MAX_HTTP_RETRIES))  # set max retries to: MAX_HTTP_RETRIES
+        s.mount("https://", requests.adapters.HTTPAdapter(max_retries = MAX_HTTP_RETRIES)) # set max retries to: MAX_HTTP_RETRIES
+        s.headers.update(HEADERS)   # set headers of the session
+
+        # send get request to the http page (if you need a post request you could also use s.post(...))
+        resp = s.get(POSTS_URL+str(pageStart))
+        # adds the html text of the http response to the BeautifulSoup parser
+        soup = bs.BeautifulSoup(resp.text, "html.parser")
+
+        # find "next page" button link - to import all the news recursive
+        nextPageLink = BASE_URL+soup.find("a", class_="paginatorAll paginatorNext")["href"]
+
+        while nextPageLink != None:
+            try:
+                pagesChecked += 1
+                # find all ~15 articles on current page
+                articles = soup.find("div", class_="fl newsListingHolder").find_all("div", class_="fl newsTitleContent")
+
+                for article in articles:
+                    articlesChecked += 1
+
+                    title = article.find("a", class_="newsTitle").text           # finds article title
+                    link = BASE_URL+"/"+article.find("a", class_="newsTitle")["href"]         # finds article http link
+                    shortDescr = article.find("a", class_="newsContent").text # finds article date (DATUM_VNOSA)
+                    hashStr = makeHash(title, shortDescr)                                      # creates article hash from title and dateStr (HASH_VREDNOST)
+                    
+
+                    # if article is not yet saved in the database we add it
+                    if sqlBase.getByHash(hashStr) is None:
+                        # get article description/content
+                        description, dateStr = getArticleDescr(s, link)
+                        date_created = uniformDateStr(dateStr, "%d.%m.%Y") # date when the article was published on the page
+                        date_downloaded = todayDateStr                     # date when the article was downloaded
+
+                        # (date_created: string, caption: string, contents: string, date: string, hash: string, url: string, source: string)
+                        entry = (date_created, title, description, date_downloaded, hashStr, link, SOURCE_ID)
+                        sqlBase.insertOne(entry, True)   # insert the article in the database
+                        articlesDownloaded += 1
+
+                    if articlesChecked % 5 == 0:
+                        logger.info("Checked: {} articles. Downloaded: {} new articles.".format(articlesChecked, articlesDownloaded))
+
+                # find next page
+                resp = s.get(nextPageLink)                        # loads next page
+                soup = bs.BeautifulSoup(resp.text, "html.parser") # add html text to the soup
+                nextPageLink = soup.find("a", class_="paginatorAll paginatorNext")
+                if nextPageLink:
+                    nextPageLink = BASE_URL+nextPageLink["href"]   # select the "next page" button http link
+                if not firstRunBool and pagesChecked >= NUM_PAGES_TO_CHECK:
+                    break
+                    
+            except Exception:
+                logger.exception("")
+                sys.exit(1)
+
+    logger.info("Downloaded {} new articles.".format(articlesDownloaded))
+
+# starts main function
+if __name__ == '__main__':
+    # checks if the second argument is provided and is equal to "-F" - means first run
+    if len(sys.argv) == 2 and sys.argv[1] == "-F":
+        firstRunBool = True
+
+    print ("Add -F as the command line argument to execute first run command - downloads the whole history of articles from the page.")
+
+    main()
---
+++ scrapers/Scraper-razvoj.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+
+import bs4 as bs
+import requests
+import re
+import hashlib
+import os.path
+import sys
+import datetime
+from logLoader import loadLogger, setLoggerLevel
+from database.dbExecutor import dbExecutor
+
+"""
+    TODO: fix database encoding
+"""
+
+SOURCE_ID = "RAZVOJ"  # source identifier
+NUM_PAGES_TO_CHECK = 1 # how many pages will we check evey day for new articles
+MAX_HTTP_RETRIES = 10  # set max number of http request retries if a page load fails
+BASE_URL = "http://www.razvoj.si"
+POSTS_URL = BASE_URL+"/?lng=sl&t=aktualno&year="
+
+MAX_YEAR = 2007
+
+firstRunBool = False   # import all the articles that exist if true; overrides NUM_PAGES_TO_CHECK
+
+logger = loadLogger(SOURCE_ID)
+setLoggerLevel(logger, "INFO")
+
+# makes a sha1 hash out of title and date strings
+# returns string hash
+def makeHash(articleTitle, dateStr):
+    hash_object = hashlib.sha1((articleTitle+dateStr).encode("utf-8"))
+    return hash_object.hexdigest()
+
+# navigates to the given link and extracts the article description
+def getArticleDescr(session, link):
+    resp = session.get(link)
+    soup = bs.BeautifulSoup(resp.text, "html.parser")
+    return soup.find("div", id="contentCenterSubjectBlock").find("span", class_=None).text
+
+# creates a uniform date string out of the input @dateStr and date format @inputDateFromat
+def uniformDateStr(dateStr, inputDateFromat=""):
+    if inputDateFromat == "":
+        inputDateFromat = "%d.%m.%Y"
+    return datetime.datetime.strptime(dateStr, inputDateFromat).strftime("%Y-%m-%d")
+
+
+def main():
+    articlesChecked = 0
+    articlesDownloaded = 0  # number of downloaded articles
+
+    sqlBase = dbExecutor()  # creates a sql database handler class
+    todayDateStr = datetime.datetime.now().strftime("%Y-%m-%d") # today date in the uniform format
+    yearInt = datetime.datetime.now().year
+
+    # optionally set headers for the http request
+    HEADERS = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+    }
+
+    with requests.Session() as s:
+        # set every http/https request to retry max MAX_HTTP_RETRIES retries before returning an error if there is any complications with loading the page
+        s.mount("http://", requests.adapters.HTTPAdapter(max_retries = MAX_HTTP_RETRIES))  # set max retries to: MAX_HTTP_RETRIES
+        s.mount("https://", requests.adapters.HTTPAdapter(max_retries = MAX_HTTP_RETRIES)) # set max retries to: MAX_HTTP_RETRIES
+        s.headers.update(HEADERS)   # set headers of the session
+
+        maxYearToCheck = MAX_YEAR-1
+        if not firstRunBool:
+            maxYearToCheck = yearInt-1
+        for yearNum in range(yearInt, maxYearToCheck, -1):
+            logger.info("Checking year: {}".format(yearNum))
+
+            yearPageLink = POSTS_URL+str(yearNum)
+
+            try: 
+                resp = s.get(yearPageLink)
+                soup = bs.BeautifulSoup(resp.text, "html.parser")
+
+                articleDates = soup.find("div", id="contentCenterSubjectBlock").find_all("span", class_="contentCenterSubjectBlockDate")
+                articleLinks = soup.find("div", id="contentCenterSubjectBlock").find_all("a")
+                if len(articleDates) != len(articleLinks):
+                    logger.error("Page is different: can not find same amount of dates and links.")
+                    sys.exit(1)
+
+                for num in range(len(articleDates)):
+                    # print (articleLinks[num]["href"])
+                    articlesChecked += 1
+
+                    title = articleLinks[num].text
+                    link = BASE_URL+"/"+articleLinks[num]["href"]
+                    dateStr = articleDates[num].text.strip(" ").strip("\n").strip("\r")
+                    hashStr = makeHash(title, dateStr)
+
+                    logger.debug("TITLE: {}".format(title.encode("utf-8")))
+                    logger.debug("TITLE: {}".format(link))
+                    logger.debug("DATE: {}".format(dateStr))
+
+                    date_created = uniformDateStr(dateStr, "%d.%m.%Y") # date when the article was published on the page
+                    date_downloaded = todayDateStr                       # date when the article was downloaded
+
+                    # if article is not yet saved in the database we add it
+                    if sqlBase.getByHash(hashStr) is None:
+                        # get article description/content
+                        description = getArticleDescr(s, link)
+
+                        # (date_created: string, caption: string, contents: string, date: string, hash: string, url: string, source: string)
+                        entry = (date_created, title, description, date_downloaded, hashStr, link, SOURCE_ID)
+                        sqlBase.insertOne(entry, True)   # insert the article in the database
+                        articlesDownloaded += 1
+
+                    if articlesChecked % 5 == 0:
+                        logger.info("Checked: {} articles. Downloaded: {} new articles.".format(articlesChecked, articlesDownloaded))
+
+            except Exception:
+                logger.exception("")
+                sys.exit(1)
+
+    logger.info("Downloaded {} new articles.".format(articlesDownloaded))
+
+
+if __name__ == '__main__':
+    # checks if the second argument is provided and is equal to "-F" - means first run
+    if len(sys.argv) == 2 and sys.argv[1] == "-F":
+        firstRunBool = True
+
+    print ("Add -F as the command line argument to execute first run command - downloads the whole history of articles from the page.")
+
+    main()
