e573dfa829aacadec297088802bedfcf1d4d55df
Dejan <dejan.ozebek@gmail.com>
2018-08-21 12:23:59 +0200

    Update about

--- spletna/SLEDIMedO/view/about.php
+++ spletna/SLEDIMedO/view/about.php
@@ -15,7 +15,15 @@
 			<div class="page-container">
 				<div class="pages-title">O projektu</div>
 				<div class="pages-content">
-					Tukaj bo pisalo nekaj o projektu. Nekaj o projektu.
+					SlediMedO - o projektu
+
+Dandanes imamo opravka z veliko kolicino informacij, kot tudi s množico medijev, ki nam te informacije posredujejo pa naj bodo to televizijski mediji, casopisi, medijske hiše, ali pa socialni mediji.
+Prav zaradi tega je vcasih težko ali pa vsaj zahtevnejše najti relavantne informacije za dolocena iskanja, kar nam lahko v koncni fazi vzame veliko casa.
+
+V okviru študentskih inovativnih projektov za družbeno korist (ŠIPK) se tako izvaja projekt 'Sledenje medijskih objav' cigar namen je prav sledenje objavam ter hranjenje potencialnih objaviteljev novic o financno podprtih projektih s strani Evropske unije (natancneje Danube Transnational Programme) in nenazadnje tudi formiranje spletne strani, ki bi omogocala iskanje teh projektov, oziroma novic o le-teh, na enostavnejši nacin.
+ 
+Pri tem smo se osredotocili na lokalne medije oziroma spletne casopise, kot tudi na socialne medije in projekte same po sebi. S pomocjo racunalniških tehnologij smo tako crpali relavantne novice o Evropskih projektih v Sloveniji.
+
 				</div>
 			</div>
 		</section>
