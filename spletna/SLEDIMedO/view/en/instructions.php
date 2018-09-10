a72756eb8bd1f0e8800df9ef6dc286aa8e34aa84
Dejan <dejan.ozebek@gmail.com>
2018-09-10 14:31:42 +0200

    update navodil, dodajanje slik

--- spletna/SLEDIMedO/view/en/instructions.php
+++ spletna/SLEDIMedO/view/en/instructions.php
@@ -16,13 +16,17 @@
 				<div class="pages-content">
 					<p>
 					This website <b>SlediMedO</b> was created to help with the search of news about different projects funded or co-funded by the European union. The search engine uses scrappers and a database to search through various Slovenian media and news websites, included ones with basic information about a given project.<br>
-					To begin your searching, click on the site 'Search' where you'll see a search bar into which you'll type your search query. You can use names of different projects or their acronyms or some key words about the project. The site will then return the results of the search, such as a main website of the project with the basic information of the project, as well as news articles from various news sites about this project. The database mostly includes projects from the Danube region.
+					To begin your searching, click on the site 'Search' where you'll see a search bar into which you'll type your search query. You can use names of different projects or their acronyms or some key words about the project. The search bar will display some projects that are related to your search query. If none of them apply, just press the search icon and the site will take you to all the related news. Each search result has the name of the article, publication date and the main text. Otherwise, you can also click on the link that will take you directly to the web page of a given news article.
 					</p>
 					<br><br>
 					<p class="page-sub-title">EXAMPLE OF USE</p>
 					<p>
-					Below you can see an example of how to use the website, with the '' project. To find the news you type key words, acronyms or full project names. The site will return different news websites with news about the project as well as websites with basic information about the project.
-					(Insert pic of use)
+					Below you can see an example of how to use the website, with the search query 'smart'. To find the news you type key words, acronyms or full project names. The site will show you some search results in a drop down menu, but if none of them are what you are searching for, just press enter and the site will take you to a subsite, where every article related to your search query is.
+					<img src="../img/slika1.png" alt="Example 1"><br>
+					In the next image we can see three selected search results. To proceed we can just choose one and click on it.
+					<img src="../img/slika2.png" alt="Example 2"><br>
+					The last picture shows the summary of the article as well as its date and direct link to the origin website of the article.
+					<img src="../img/slika3.png" alt="Example 3"><br>
 					</p>
 				</div>
 			</div>
--- spletna/SLEDIMedO/view/instructions.php
+++ spletna/SLEDIMedO/view/instructions.php
@@ -16,13 +16,18 @@
 				<div class="pages-content">
 					<p>
 					Spletna stran <b>SlediMedO</b> je bila ustvarjena za iskanje različnih novic o projektih financiranih s strani Evropske unije. Hkrati ustvarjen iskalnik poskrbi tudi, da se iščejo rezultati oziroma novice z različnih spletnih strani. Med temi stranmi so vključene tudi osnovne strani o določenem projektu ter različni slovenski spletni viri.<br>
-					Za uporabo kliknite na podstran 'Iskanje', kjer se vam bo prikazal okvir za vpis ključnih besed. Za iskalni ukaz lahko vpišete imena različnih projektov, njihove kratice oziroma približke imen projektov. Stran bo poskrbela, da vam vrne vse novice, ki so povezane z danim projektom, kot tudi strani z informacijami o določenem projektu. V podatkovni bazi so vključeni projekti iz Podonavske regije.
+					Za uporabo kliknite na podstran 'Iskanje', kjer se vam bo prikazal okvir za vpis ključnih besed. Za iskalni ukaz lahko vpišete imena različnih projektov, njihove kratice oziroma približke imen projektov ali pa ključne besede. Iskalnik vam že ob takojšnjem pisanju predlaga določene novice ali projekte, ki se ujemajo z vašim iskalnim nizom. Tako lahko izberete med njimi, ali pa preprosto pritisnite tipko enter. Stran vam bo nato prikazala podstran, kjer bodo zbrani rezultati, ki se ujemajo z vašim iskalnim nizom. Vsak rezultat iskanja ima svoj naslov ter datum objave, hkrati pa, če kliknete na določen rezultat, vas stran preusmeri na podstran, kjer je zbrana novica, poleg tega pa lahko tudi direktno obiščete spletno stran, kjer je bila novica objavljena.
 					</p>
 					<br><br>
 					<p class="page-sub-title">PRIMER UPORABE</p>
 					<p>
-					Spodaj je prikazan primer uporabe spletna strani za projekt ''. V iskalni okvir vpišete ključne besede, stran pa vam prikaže podstran, kjer so zbrane različne strani, ki vključujejo osnovne podatke o danem projektu, ter različne novice iz različnih slovenskih medijev (spletnih strani).
-					(Dodaj sliko/e uporabe)
+					Spodaj je prikazan primer uporabe spletna strani za iskalni niz 'smart'. V iskalni okvir vpišete ključne besede, spletna stran vam medtem prikaže nekaj potencialnih zadetkov, sicer pa vam nato na podstrani prikaže vse zadetke, ki se ujemajo z vašim iskalnim nizom.<br>
+					Na prvi sliki vidimo iskalnik ter predloge spletne strani, ki jih dobimo za dano ključno besedo.
+					<img src="/img/slika1.png" alt="Primer 1"><br>
+					Zatem vidimo tri zadetke oziroma novice, ki jih vrne iskalni niz. Da odpremo naslednjo stran, preprosto kliknemo na izbrano novico.
+					<img src="/img/slika2.png" alt="Primer 2"><br>
+					Na zadnji sliki je prikazana izbrana novica. Pri tem lahko vidimo izbrani tekst novice, hkrati pa tudi datum objave novice in direktno povezavo na spletno stran, kjer je novica objavljena.
+					<img src="/img/slika3.png" alt="Primer 3"><br>
 					</p>
 				</div>
 			</div>
