d0d5107855cd8e4f52ca07062d29f06047b0c811
Dejan <dejan.ozebek@gmail.com>
2018-09-13 11:37:34 +0200

    popravki

--- spletna/SLEDIMedO/view/about.php
+++ spletna/SLEDIMedO/view/about.php
@@ -16,21 +16,21 @@
 				<div class="pages-content">
 					<p class="page-sub-title">SLEDILEC MEDIJSKIH OBJAV</p>
 					<p>
-					Republika Slovenija je kot clanica EU aktivno vkljucena v izvajanje strategij treh makroregij - Podonavske, Jadransko-jonske in Alpske. Vsem trem makroregijam je skupno, da informacije o rezultatih projektov, ki se financirajo preko razlicnih instrumentov EU, ne pridejo do državljanov, kljub temu, da so le ti najveckrat projektni deležniki. Poleg tega se o aktivnostih in rezultatih premalo poroca v splošnih nacionalnih in lokalnih medijih, najsibodo to spletni ali klasicni mediji.
+					Republika Slovenija je kot članica EU aktivno vključena v izvajanje strategij treh makroregij - Podonavske, Jadransko-jonske in Alpske. Vsem trem makroregijam je skupno, da informacije o rezultatih projektov, ki se financirajo preko različnih instrumentov EU, ne pridejo do državljanov, kljub temu, da so le ti največkrat projektni deležniki. Poleg tega se o aktivnostih in rezultatih premalo poroča v splošnih nacionalnih in lokalnih medijih, najsibodo to spletni ali klasični mediji.
 					</p>
 					<br>
 					<p>
-					Dandanes imamo opravka z veliko kolicino informacij, kot tudi s množico medijev, ki nam te informacije posredujejo pa naj bodo to televizijski mediji, casopisi, medijske hiše, ali pa socialni mediji.
-					Prav zaradi tega je vcasih težko ali pa vsaj zahtevnejše najti relavantne informacije za dolocena iskanja, kar nam lahko v koncni fazi vzame veliko casa. Vecina projektov sicer ima svojo spletno stran in pogosto tudi profile na socialnih omrežjih, preko katerih zainteresirane obve?šcajo o svojih dejavnostih. A nemalokrat se tu prenos informacij ustavi, s cimer pomembno izgubijo vsi vpleteni - tako izvajalci projektov, ker se o njihovem delu premalo poroca, kot potencialni koristniki rezultatov projekta, ki ne dobijo informacij o možnostih, ki jih izvedeni projekti odpirajo. 
+					Dandanes imamo opravka z veliko količino informacij, kot tudi s množico medijev, ki nam te informacije posredujejo pa naj bodo to televizijski mediji, časopisi, medijske hiše, ali pa socialni mediji.
+					Prav zaradi tega je včasih težko ali pa vsaj zahtevnejše najti relavantne informacije za določena iskanja, kar nam lahko v končni fazi vzame veliko časa. Večina projektov sicer ima svojo spletno stran in pogosto tudi profile na socialnih omrežjih, preko katerih zainteresirane obveščajo o svojih dejavnostih. A nemalokrat se tu prenos informacij ustavi, s čimer pomembno izgubijo vsi vpleteni - tako izvajalci projektov, ker se o njihovem delu premalo poroča, kot potencialni koristniki rezultatov projekta, ki ne dobijo informacij o možnostih, ki jih izvedeni projekti odpirajo. 
 					</p>
 					<br>
 					<p>
-					Glavni namen našega projekta je bilo preuciti možnosti ugotavljanja sledljivosti medijskim objavam s podrocja izvajanja EU projektov. Pri tem gre za uporabo inovativnih tehnologij, ki bi omogocale semi-avtomatsko analizo sledljivosti, preko kreativnega graficnega uporabni?škega vmesnika. V okviru Študentskih inovativnih projektov za družbeno korist (ŠIPK) se tako izvaja projekt 'Sledenje medijskih objav' cigar namen je prav sledenje objavam ter hranjenje potencialnih objaviteljev novic o financno podprtih projektih s strani Evropske unije (natancneje Danube Transnational Programme) in nenazadnje tudi formiranje spletne strani, ki bi omogocala iskanje teh projektov, oziroma novic o le-teh, na enostavnejši in prijetnejši nacin.
+					Glavni namen našega projekta je bilo preučiti možnosti ugotavljanja sledljivosti medijskim objavam s podrocja izvajanja EU projektov. Pri tem gre za uporabo inovativnih tehnologij, ki bi omogočale semi-avtomatsko analizo sledljivosti, preko kreativnega graficnega uporabniškega vmesnika. V okviru Študentskih inovativnih projektov za družbeno korist (ŠIPK) se tako izvaja projekt 'Sledenje medijskih objav' čigar namen je prav sledenje objavam ter hranjenje potencialnih objaviteljev novic o finančno podprtih projektih s strani Evropske unije (natancneje Danube Transnational Programme) in nenazadnje tudi formiranje spletne strani, ki bi omogočala iskanje teh projektov, oziroma novic o le-teh, na enostavnejši in prijetnejši nacin.
 					</p>
 					<br>
 					<p>
-					Pri tem smo se osredotocili na lokalne medije oziroma spletne casopise, kot tudi na socialne medije in projekte same po sebi. S pomocjo razlicnih racunalniških tehnologij smo tako crpali relavantne novice o Evropskih projektih v Sloveniji.
-					Rezultat projekta je celovita IKT rešitev, ki omogoca sledenje medijski pokritosti aktivnosti projektov makroregij, v katerih sodelujejo slovenski projektni partnerji. Programsko orodje omogoca hitro in ucinkovito iskanje, pregledovanje, filtriranje in obdelavo medijskih objav izbranih slovenskih virov.
+					Pri tem smo se osredotočili na lokalne medije oziroma spletne časopise, kot tudi na socialne medije in projekte same po sebi. S pomočjo razlicnih računalniških tehnologij smo tako črpali relavantne novice o Evropskih projektih v Sloveniji.
+					Rezultat projekta je celovita IKT rešitev, ki omogoča sledenje medijski pokritosti aktivnosti projektov makroregij, v katerih sodelujejo slovenski projektni partnerji. Programsko orodje omogoča hitro in učinkovito iskanje, pregledovanje, filtriranje in obdelavo medijskih objav izbranih slovenskih virov.
 					</p>
 					<br>
 					<p class="page-sub-title">O CENTRU ZA EVROPSKO PRIHODNOST</p>
@@ -39,13 +39,13 @@
 					</p>
 					<br>
 					<p>
-					Za ta namen organizirajo razne treninge in delavnice, konference in okrogle mize. Prav tako financirajo razlicne projekte pri katerih tudi sami sodelujejo.
-					Trudijo se za regionalno stabilnost in blaginjo preko gradenj novih poznanstev ter s krepitvijo že obstojecih vez z drugimi državami, da bi dosegli vecjo prepoznavnost slovenskih regij.
-					CEP je do sedaj izvedel že preko 200 projektov ter sodeloval s preko 1800 strokovnjaki. Glavne karakteristike njihovih aktivnosti pa so multidisciplinarnost in interaktivnost. Tako se s pomocjo strokovnjakov, ki so vkljuceni že pri snovanju projekta, spopadajo s razlicnimi temami, h katerim pristopajo z vecih vidikov, velikokrat tudi preko pripravnikov, ki pa tako dobijo dodatne spretnosti in znanja.
+					Za ta namen organizirajo razne treninge in delavnice, konference in okrogle mize. Prav tako financirajo različne projekte pri katerih tudi sami sodelujejo.
+					Trudijo se za regionalno stabilnost in blaginjo preko gradenj novih poznanstev ter s krepitvijo že obstojecih vez z drugimi državami, da bi dosegli večjo prepoznavnost slovenskih regij.
+					CEP je do sedaj izvedel že preko 200 projektov ter sodeloval s preko 1800 strokovnjaki. Glavne karakteristike njihovih aktivnosti pa so multidisciplinarnost in interaktivnost. Tako se s pomočjo strokovnjakov, ki so vkljuceni že pri snovanju projekta, spopadajo s različnimi temami, h katerim pristopajo z večih vidikov, velikokrat tudi preko pripravnikov, ki pa tako dobijo dodatne spretnosti in znanja.
 					</p>
 					<br>
 					<p>
-					Center za evropsko prihodnost je kot koordinator enega od prioritetnih podrocij (PA10) aktivno vkljucen v izvajanje Podonavske makroregije. Glavna naloga je koordinacija in poznavanje projektov s podrocja institucionalnega povezovanja in sodelovanja, zato ga še posebej zanima porocanje in sledenje o projektnih aktivnostih. Rezultat bo predstavljen na dogodku namenjenemu vsem strokovnjakom komuniciranja o makroregijah v Portorožu, septembra 2018 (Teden sredozemske obale, razprava 'Sporocilo v steklenici: komunikacija v makro-regijah'), tako da bo podobno rešitev lahko uporabil kakšen drug koordinator ali zainteresiran za delovanje slovenskih partnerjev v projektih makroregij.
+					Center za evropsko prihodnost je kot koordinator enega od prioritetnih podrocij (PA10) aktivno vkljucen v izvajanje Podonavske makroregije. Glavna naloga je koordinacija in poznavanje projektov s področja institucionalnega povezovanja in sodelovanja, zato ga še posebej zanima porocanje in sledenje o projektnih aktivnostih. Rezultat bo predstavljen na dogodku namenjenemu vsem strokovnjakom komuniciranja o makroregijah v Portorožu, septembra 2018 (Teden sredozemske obale, razprava 'Sporočilo v steklenici: komunikacija v makro-regijah'), tako da bo podobno rešitev lahko uporabil kakšen drug koordinator ali zainteresiran za delovanje slovenskih partnerjev v projektih makroregij.
 					</p>
 					<br><br>
 					<p class="page-sub-title">SODELUJOCI</p>
